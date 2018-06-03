78fa9f8c9da8a9ec14b795029adee056f7e44002
constantine covtushenko <constantine.cov@gmail.com>
2018-06-03 10:52:09 -0400

    added index page, styles

--- django_sample/django_sample/urls.py
+++ django_sample/django_sample/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
 
 urlpatterns = [
+    url(r'^$', index, name='index'),
+    url(r'^index|index.html$', index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
 ]
